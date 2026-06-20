2c60f3f2e150cc0deab959f048006f795c04539d
OCOS UK <support@ocos.io>
2026-06-21 02:40:46 +0400

    Create MockERC20.sol

---
+++ contracts/test/mocks/MockERC20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    constructor() ERC20("Mock USDC", "mUSDC") {
+        _mint(msg.sender, 1_000_000_000 ether);
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
