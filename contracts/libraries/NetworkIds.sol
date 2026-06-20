690cb35a6fa0500a3c60f0ff5170924ecb4408bf
OCOS UK <support@ocos.io>
2026-06-21 02:36:34 +0400

    Create NetworkIds.sol

---
+++ contracts/libraries/NetworkIds.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.24;
+
+library NetworkIds {
+    uint256 internal constant ETHEREUM = 1;
+
+    uint256 internal constant BNB = 56;
+
+    uint256 internal constant POLYGON = 137;
+
+    uint256 internal constant AVALANCHE = 43114;
+
+    uint256 internal constant ARBITRUM = 42161;
+
+    uint256 internal constant OPTIMISM = 10;
+
+    uint256 internal constant BASE = 8453;
+
+    uint256 internal constant FANTOM = 250;
+
+    uint256 internal constant ZKSYNC = 324;
+
+    uint256 internal constant LINEA = 59144;
+
+    uint256 internal constant SCROLL = 534352;
+
+    uint256 internal constant SOLANA = 10001;
+
+    uint256 internal constant TRON = 10002;
+
+    uint256 internal constant APTOS = 10003;
+
+    uint256 internal constant SUI = 10004;
+
+    uint256 internal constant NEAR = 10005;
+
+    uint256 internal constant COSMOS = 10006;
+
+    uint256 internal constant TON = 10007;
+
+    uint256 internal constant BITCOIN = 10008;
+}
